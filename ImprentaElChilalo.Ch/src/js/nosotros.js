ae6bebb4d720b239e15523c69c013692ad879705
alejandrozapata0304 <117397892+alejandrozapata0304@users.noreply.github.com>
2023-06-27 14:52:07 -0500

    Implementacion del mapa, se agrego codigo a nosotros.js

--- ImprentaElChilalo.Ch/src/js/nosotros.js
+++ ImprentaElChilalo.Ch/src/js/nosotros.js
@@ -2,3 +2,20 @@
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
+function initMap() {
+    var location = { lat: 40.712776, lng: -74.005974 }; // Coordenadas de ejemplo (Nueva York)
+    var mapOptions = {
+      center: location,
+      zoom: 12
+    };
+  
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      title: 'Ubicación'
+    });
+  }
+  
+  
